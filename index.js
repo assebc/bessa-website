6e92a6f0d2660ef0833c2b5eef9c1d86ea535461
assebc <claudioafbessa@gmail.com>
2022-08-24 12:27:03 +0100

    :heayv_minus_sign: removed text

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 
 let sun = document.getElementById('sun');
 let behind = document.getElementById('behind');
-let text = document.getElementById('text');
 let front = document.getElementById('front');
 let header = document.querySelector('header');
 
@@ -10,7 +9,5 @@ window.addEventListener('scroll', function(){
     sun.style.top = value * 1.05 + 'px';
     behind.style.top = value * 0.5 + 'px';
     front.style.top = value * 0.5 + 'px';
-    text.style.marginRight = value * 7 + 'px';
-    text.style.marginTop = value * 1.5 + 'px';
     header.style.top = value * 0.25 + 'px';
 })
