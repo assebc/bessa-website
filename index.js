ad1b0dad9834c7aa655d428490550742597c72a8
assebc <claudioafbessa@gmail.com>
2022-08-24 20:44:03 +0100

    :wrench: sun 10px to the right fixed

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 let sun = document.getElementById('sun');
 let behind = document.getElementById('behind');
 let front = document.getElementById('front');
@@ -6,7 +5,7 @@ let header = document.querySelector('header');
 
 window.addEventListener('scroll', function(){
     let value = window.scrollY;
-    sun.style.top = value * 1 + 'px';
+    sun.style.top = value * 1.05 + 'px';
     behind.style.top = value * 0.5 + 'px';
     front.style.top = value * 0.5 + 'px';
     header.style.top = value * 0.25 + 'px';
