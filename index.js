a7705e513251762a6bfdcdfb3363d16ce43d3b68
assebc <claudioafbessa@gmail.com>
2022-08-24 12:10:13 +0100

    :wrench: updated javascripts ids and factors

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let stars = document.getElementById('stars');
-let moon = document.getElementById('moon');
+
+let sun = document.getElementById('sun');
 let behind = document.getElementById('behind');
 let text = document.getElementById('text');
 let front = document.getElementById('front');
@@ -7,11 +7,10 @@ let header = document.querySelector('header');
 
 window.addEventListener('scroll', function(){
     let value = window.scrollY;
-    stars.style.left = value * 0.25 + 'px';
-    moon.style.top = value * 1.05 + 'px';
+    sun.style.top = value * 1.05 + 'px';
     behind.style.top = value * 0.5 + 'px';
-    front.style.top = value * 0 + 'px';
-    text.style.marginRight = value * 4 + 'px';
+    front.style.top = value * 0.5 + 'px';
+    text.style.marginRight = value * 7 + 'px';
     text.style.marginTop = value * 1.5 + 'px';
     header.style.top = value * 0.25 + 'px';
 })
