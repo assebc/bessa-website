6c893c4c509e5493b50f6cabdb46601f633e6438
assebc <claudioafbessa@gmail.com>
2022-08-24 20:49:39 +0100

    :bug: trying to insert js into html

--- aboutme/aboutme.js
+++ aboutme/aboutme.js
@@ -1,5 +1,7 @@
 function calculateAge(birthday) { // birthday is a date
+    let age = document.getElementById("age");
     var ageDifMs = Date.now() - birthday;
     var ageDate = new Date(ageDifMs); // miliseconds from epoch
-    return Math.abs(ageDate.getUTCFullYear() - 2002);
+    age.innerHTML = Math.abs(ageDate.getUTCFullYear() - 2002);
+    return age;
   }
