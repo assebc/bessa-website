3a910618ca35569df0c6752c94526008bc9dad93
assebc <claudioafbessa@gmail.com>
2022-08-26 00:37:22 +0100

    :heavy_minus:sign: removed javascript file

--- aboutme/aboutme.js
+++
@@ -1,7 +0,0 @@
-function calculateAge(birthday) { // birthday is a date
-    let age = document.getElementById("age");
-    var ageDifMs = Date.now() - birthday;
-    var ageDate = new Date(ageDifMs); // miliseconds from epoch
-    age.innerHTML = Math.abs(ageDate.getUTCFullYear() - 2002);
-    return age;
-  }
