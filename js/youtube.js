426373665ca518e637aec6f95ec20dbf6afff611
assebc <claudioafbessa@gmail.com>
2022-09-13 20:36:44 +0100

    :heavy_plus_sign: default youtube video link

--- js/youtube.js
+++ js/youtube.js
@@ -1,7 +1,9 @@
 var xml_link = "https://www.youtube.com/feeds/videos.xml?channel_id=UCYXW07u06W_eyY_GMJcQDaw"
 var nodes = doc.getElementsByTagName("yt_videoId");
-var videos = [];
+var videos_Id = [];
+var yt_link = "https://www.youtube.com/watch?v="; // concat the the video_Id link
 
 for(var i=0;i<nodes.length;i++){
-    videos.push(nodes.item(i).innerHTML);
+    videos_Id.push(nodes.item(i).innerHTML);
 }
+
